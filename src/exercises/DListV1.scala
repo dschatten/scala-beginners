2c99733fa0ffb8dca7fbf5de6e4da2d2ba0acf88
dschatten <drschatten@gmail.com>
2020-03-24 15:19:37 -0400

    Add recursive print method.

--- src/exercises/DListV1.scala
+++ src/exercises/DListV1.scala
@@ -11,7 +11,7 @@ abstract class DListV1 {
   def isEmpty: Boolean
   def add(element: Integer): DListV1
 
-//  override protected def toString: String = super.toString
+  override def toString: String = super.toString
 }
 
 //Define as object, doesn't need to be instantiated
@@ -20,6 +20,8 @@ object DEmptyListV1 extends DListV1{
   def tail: DListV1 = throw new NoSuchElementException
   def isEmpty: Boolean = true
   def add(element: Integer): DListV1 = new DConsV1(element, DEmptyListV1)
+
+  override def toString: String = " "
 }
 
 class DConsV1(h: Int, t: DListV1) extends DListV1{
@@ -27,6 +29,10 @@ class DConsV1(h: Int, t: DListV1) extends DListV1{
   def tail: DListV1 = t
   def isEmpty: Boolean = false
   def add(element: Integer): DConsV1 = new DConsV1(element, this)
+
+  override def toString: String = {
+    s"$head " + this.tail.toString
+  }
 }
 
 object Driver extends App{
@@ -34,6 +40,12 @@ object Driver extends App{
     println(aList.head)
 
   //Create a linked list
-  var ll = new DConsV1(3, new DConsV1(2, new DConsV1(1, DEmptyListV1)))
+  val ll = new DConsV1(3, new DConsV1(2, new DConsV1(1, DEmptyListV1)))
   println(ll.head)
+
+  //Test the add method
+  println(s"LL head ${ll.add(20).head}")
+
+  //Test the toString method
+  println(s"Printing to string: ${ll.toString}")
 }
