cae7ee5b4eff6c5a2a394561bc1ff419f08958ce
dschatten <drschatten@gmail.com>
2020-03-30 20:26:17 -0400

    Recursive pattern-match for math expressions example.

--- src/lectures/part4pm/PatternMatching.scala
+++ src/lectures/part4pm/PatternMatching.scala
@@ -69,10 +69,30 @@ object PatternMatching extends App {
      Prod(Sum(Number(2), Number(1)), Number(3)) = (2 + 1) * 3
      Sum(Prod(Number(2), Number(1)), Number(3)) = 2 * 1 + 3
    */
-  trait Expr
+  trait Expr //some math expresion
   case class Number(n: Int) extends Expr
   case class Sum(e1: Expr, e2: Expr) extends Expr
   case class Prod(e1: Expr, e2: Expr) extends Expr
+
+  val myExpr = Prod(Sum(Number(3), Number(4)), Number(2))
+
+  def printItOut(expr: Expr): String = {
+    expr match {
+      case Number(n) => s"$n"
+      case Sum(e1, e2) => "(" + printItOut(e1) + " + " + printItOut(e2) + ")"
+      case Prod(e1, e2) => "(" + printItOut(e1) + " * " + printItOut(e2) + ")"
+    }
+  }
+    println(s"Print it out: ${printItOut(myExpr)}")
+
+
+
+
+
+
+
+
+
 
   def show(e: Expr): String = e match {
     case Number(n) => s"$n"
